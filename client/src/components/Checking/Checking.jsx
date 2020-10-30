1fd9d505053fa46cf58aca0e311c5782857a2966
omegadesigner <omegadesigner@hotmail.com>
2020-10-30 11:06:27 -0400

    Cabe - Component Boilerplates

---
+++ client/src/components/Checking/Checking.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Checking(props) {
+    return (
+        <div>
+            <div>My Checking</div>
+            <button>Edit Button Placeholder</button>
+            <ul>
+                <li>000-0000-00000000 Checking</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Checking;
---
+++ client/src/components/Rewards/Rewards.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Rewards(props) {
+    return (
+        <div>
+            <div>My Rewards</div>
+            <form className="rewards-dropdown">
+                <label htmlFor="rewards">Rewards:</label>
+                <select className="rewards" onChange={handleChange}>
+                    <option value="1-5" >&nbsp; 1-5% Cash Back &nbsp;</option>
+                    <option value="5-10">&nbsp; 5-10% Cash Back &nbsp;</option>
+                </select>
+            </form>
+        </div>
+    );
+}
+
+export default Rewards;
