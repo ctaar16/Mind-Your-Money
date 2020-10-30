d8b5687dd4eba5151dc5e7e62083adac21cc6a25
jozillasaurus <jozilla3@gmail.com>
2020-10-30 11:28:24 -0700

    navbar-links

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Nav.css'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Nav = () => {
 
     return (
       <nav>
         <div className="nav">
-        <NavLink className="logo" to="/">Mind Yo' Money!</NavLink>
-                
-            </div>
-        </nav>
+          <NavLink className="logo" to="/">Mind Yo' Money!</NavLink>
+          <Link>
+          <img src={process.env.PUBLIC_URL + '/Goals.png'} />
+          </Link>
+         </div>
+       </nav>
     )
 
 }
