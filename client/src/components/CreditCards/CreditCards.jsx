ccd329d66f551b7eed8bd2d91372506fe85b3bd3
omegadesigner <omegadesigner@hotmail.com>
2020-11-02 11:57:03 -0500

    Cabe - Typo Fix

--- client/src/components/CreditCards/CreditCards.jsx
+++ client/src/components/CreditCards/CreditCards.jsx
@@ -6,13 +6,13 @@ function CreditCards(props) {
     return (
         <div className="card-layout">
             <div className="card-title-bar">
-                <div className="card-title">My Crdit Cards</div>
+                <div className="card-title">My Credit Cards</div>
                 <NavLink className="link" to="/EditAccount">
                     <img className="card-button" src={process.env.PUBLIC_URL + 'assets/Edit-Icon.png'} alt="Edit"/>
                 </NavLink>
             </div>
             <div className="card-list">
-                <div className="card-item">000-0000-00000000 Crdit Cards</div>
+                <div className="card-item">000-0000-00000000 Credit Cards</div>
             </div>
         </div>
     );
